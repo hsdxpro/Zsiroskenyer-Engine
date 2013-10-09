813963ed8cabea8dc52a64bd4bba68a415f51fc8
petiaccja <kardospeter1994@hotmail.com>
2013-10-09 17:47:29 +0200

    RendererD3D11 and GraphicsEngine projects added, RendererD3D11 DLL entry for windows written

---
+++ ZsiroskenyerEngine/RendererD3D11/src/DllMain.cpp
@@ -0,0 +1,34 @@
+////////////////////////////////////////////////////////////////////////////////
+//	File: DllMain.cpp
+//	Created:
+//		2013. oct. 09.
+//		by Péter Kardos
+////////////////////////////////////////////////////////////////////////////////
+//	Content:
+//		Windows boilerplate DllMain function.
+//
+////////////////////////////////////////////////////////////////////////////////
+
+
+#ifdef _WIN32
+
+#include <Windows.h>
+#include <tchar.h>
+
+
+BOOL WINAPI DllMain(HINSTANCE hInstDll, DWORD reason, void* reserved) {
+	switch (reason) {
+	case DLL_PROCESS_ATTACH:
+		return TRUE;;
+	case DLL_PROCESS_DETACH:
+		return TRUE;;
+	case DLL_THREAD_ATTACH:
+		break;
+	case DLL_THREAD_DETACH:
+		break;
+	}
+	return TRUE;
+}
+
+
+#endif // #ifdef WIN_32
