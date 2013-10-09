0b85cdfd4efca47e0a6b3ff0b225e4d3093238e6
HSDXPRO <hsdxpro@gmail.com>
2013-10-10 00:06:08 +0200

    Reforming Colorf

--- ZsiroskenyerEngine/CommonLib/src/math/color.h
+++ ZsiroskenyerEngine/CommonLib/src/math/color.h
@@ -1,3 +1,4 @@
+// Color.h By Zsíroskenyér Team 2013.10.10 0:00 last modified Németh Richárd
 /////////////////////////////////////////////////////
 //	Colorf ---> float4
 //	Color ----> DWORD
@@ -5,47 +6,31 @@
 /////////////////////////////////////////////////////
 #pragma once
 
-
-
 struct Colorf {
 	public:
-		// members
 		float r,g,b,a;
 
-		// constructors
-		Colorf() {a=r=g=b=0.f;}
-		Colorf(float m) {a=1.f; r=g=b=m;}
-		Colorf(float r, float g, float b, float a) {
-			this->a = a;
-			this->r = r;
-			this->g = g;
-			this->b = b;
-		}
-		Colorf(float r, float g, float b) {
-			this->a = 1.f;
-			this->r = r;
-			this->g = g;
-			this->b = b;
-		}
+		Colorf();
+		Colorf(float m);
+		Colorf(float r, float g, float b, float a);
+		Colorf(float r, float g, float b);
 
-		// operators
-		Colorf& operator*=(const Colorf& c);
-		Colorf& operator/=(const Colorf& c);
-		Colorf& operator+=(const Colorf& c);
-		Colorf& operator-=(const Colorf& c);
-
-		Colorf& operator*=(const float& s);
-		Colorf& operator/=(float s);
-
-		Colorf operator*(const Colorf& c);
-		Colorf operator/(const Colorf& c);
-		Colorf operator+(const Colorf& c);
-		Colorf operator-(const Colorf& c);
-
-		Colorf operator*(const float& s);
-		Colorf operator/(const float& s);
-
-		// methods
 		float lightness();
 		Colorf& tomonochrome();
+
+		Colorf& operator *= (const Colorf& c);
+		Colorf& operator /= (const Colorf& c);
+		Colorf& operator += (const Colorf& c);
+		Colorf& operator -= (const Colorf& c);
+
+		Colorf& operator *= (const float& s);
+		Colorf& operator /= (float s);
+
+		Colorf operator * (const Colorf& c);
+		Colorf operator / (const Colorf& c);
+		Colorf operator + (const Colorf& c);
+		Colorf operator - (const Colorf& c);
+
+		Colorf operator * (const float& s);
+		Colorf operator / (const float& s);
 };
