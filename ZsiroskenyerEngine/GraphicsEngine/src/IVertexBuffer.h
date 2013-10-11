fb99fe6a6120dcc5aa1e0c79ab02fb09528e1c4b
HSDXPRO <hsdxpro@gmail.com>
2013-10-12 01:41:02 +0200

    reimporting source

--- ZsiroskenyerEngine/GraphicsEngine/src/IVertexBuffer.h
+++
@@ -1,7 +0,0 @@
-// IVertexBuffer.h By Zsíroskenyér Team 2013.10.11 2:19 last modified: Richárd Németh
-// Responsibility: Holding Mesh points of the 3D object
-#pragma once
-
-class IVertexBuffer {
-
-};
