c2cdaa18bbdfc19feb5fd2d2259691fa43717603
petiaccja <kardospeter1994@hotmail.com>
2013-10-14 15:10:15 +0200

    BASZD MEG

---
+++ ZsiroskenyerEngine/GraphicsCommon/src/Entity.h
@@ -0,0 +1,28 @@
+////////////////////////////////////////////////////////////////////////////////
+//	File: GraphicsEngine/src/Entity.h
+//	2013.oct.09, Zsiroskenyer Team, Péter Kardos
+////////////////////////////////////////////////////////////////////////////////
+//	A class that represents an entity on the 3D scene.
+////////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include "..\..\CommonLib\src\math\math_all.h"
+
+
+class cInstanceGroup;
+
+
+
+class cEntity {
+	friend class cManagerScene;
+public:
+	cEntity();
+
+	Vec3 position;
+	Quat rotation;
+	Vec3 scale;
+	bool isVisible;
+private:
+	cInstanceGroup* instanceGroup;
+};
