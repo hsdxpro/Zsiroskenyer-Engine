003b726a5d0dd103bbf6cedef304e347eba1ae32
petiaccja <kardospeter1994@hotmail.com>
2013-10-15 16:04:13 +0200

    asd

--- ZsiroskenyerEngine/GraphicsEngine/src/GraphicsEngine.cpp
+++ ZsiroskenyerEngine/GraphicsEngine/src/GraphicsEngine.cpp
@@ -11,8 +11,12 @@
 #include "ManagerResource.h"
 #include "ManagerShader.h"
 #include "../../GraphicsCommon/src/IGraphicsApi.h"
+#include "../../GraphicsCommon/src/IShaderProgram.h"
 
 #include "../../GraphicsCommon/src/Exception.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h> // töröld ki de gecigyorsan amint nem kell debughoz
 
 
 // construction of the graphics engine
@@ -21,8 +25,13 @@ cGraphicsEngine::cGraphicsEngine() {
 	if (!gApi)
 		throw UnknownErrorException("failed to create graphics api");
 	managerShader = new cManagerShader(gApi);
-	managerResource = new cManagerResource();
+	managerResource = new cManagerResource(gApi);
 	managerScene = new cManagerScene(*managerResource);
+
+	IShaderProgram* sh = managerShader->LoadShader(L"../GraphicsEngine/shaders/",L"test");
+	if (!sh) {
+		MessageBoxA(NULL, "nincsen shader bazmeg", "F*ck", MB_OK|MB_ICONERROR);
+	}
 }
 
 
@@ -31,11 +40,17 @@ void cGraphicsEngine::RenderSceneForward() {
 	gApi->SetRenderTargetDefault();
 
 	// Set Effect...
+	auto shader = managerShader->GetShaderByName(L"test.cg");
 	
 
 	// Render each instanceGroup
 	auto instanceGroups = managerScene->GetInstanceGroups();
 
+	for (auto& group : instanceGroups) {
+		for (auto& entity : group->entities) {
+			// draw entity
+		}
+	}
 }
 
 // interface
