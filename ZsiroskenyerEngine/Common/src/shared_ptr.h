206d41be5669a709350b4893a667866fd70bc1e1
petiaccja <kardospeter1994@hotmail.com>
2013-10-21 19:51:18 +0200

    spinlock

--- ZsiroskenyerEngine/Common/src/shared_ptr.h
+++ ZsiroskenyerEngine/Common/src/shared_ptr.h
@@ -12,8 +12,24 @@
 
 template <class T>
 class zs_shared_ptr {
+public:
+	// construct
+	zs_shared_ptr();
+	template <class Y>
+	explicit zs_shared_ptr(Y* ptr);
+	template <class Y, class Deleter>
+	zs_shared_ptr(Y* ptr, Deleter d);
+	// destruct
+	~zs_shared_ptr();
 
-
-
-
+	// assignement
+	zs_shared_ptr& operator=(const zs_shared_ptr& r);
+	template <class Y>
+	zs_shared_ptr& operator=(const zs_shared_ptr<Y>& r);
+	zs_shared_ptr& operator=(zs_shared_ptr&& r);
+	template <class Y>
+	zs_shared_ptr& operator=(zs_shared_ptr<Y>&& r);
+private:
+	T* ptr;
+	static std::unordered_map<T*, size_t> counts;
 };
