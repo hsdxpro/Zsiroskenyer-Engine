9fce40e769eff0b1845a6d5080d3012c3df0e497
HSDXPRO <hsdxpro@gmail.com>
2013-10-22 13:47:01 +0200

    wasd

--- ZsiroskenyerEngine/TechDemo/src/ricsiTestFile.cpp
+++ ZsiroskenyerEngine/TechDemo/src/ricsiTestFile.cpp
@@ -36,7 +36,7 @@ int ricsiMain() {
 	std::vector<cEntity*> entities;
 	for(size_t i = 0; i < 1; i++)
 		for(size_t j = 0; j < 1 ; j++) {
-			cEntity& e =  mgrScene->AddEntity(L"objects/box.dae", L"materisl/test.zsm");
+			cEntity& e =  mgrScene->AddEntity(L"objects/box.dae", L"materials/test.zsm");
 			entities.push_back(&e);
 			e.position = Vec3(i * 10, 90, j * 10);
 			e.isVisible = true;
