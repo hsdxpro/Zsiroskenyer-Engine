81403d2f186230cf74073a220dba8819cfc410b9
Richárd Németh <hsdxpro@gmail.com>
2013-12-17 23:40:11 +0100

    asd, try more accurate specular calculation

--- ZsiroskenyerEngine/GraphicsApiD3D11/src/GraphicsApiD3D11.cpp
+++ ZsiroskenyerEngine/GraphicsApiD3D11/src/GraphicsApiD3D11.cpp
@@ -1196,16 +1196,16 @@ void cGraphicsApiD3D11::Clear(bool target /*= true*/, bool depth /*= false*/, bo
 
 void cGraphicsApiD3D11::ClearTexture(ITexture2D* t, unsigned clearFlag /*= 0*/, const Vec4& clearColor /*= Vec4()*/, float depthVal /*= 1.0f*/, size_t stencilVal /*= 0*/) {
 	ID3D11DepthStencilView* dsv = ((cTexture2DD3D11*)t)->GetDSV();
-	if (dsv != NULL)
+	if (dsv)
 		d3dcon->ClearDepthStencilView(dsv, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, depthVal, (UINT8)stencilVal);
 
 	ID3D11RenderTargetView* rtv = ((cTexture2DD3D11*)t)->GetRTV();
-	if ( rtv != NULL)
+	if (rtv)
 		d3dcon->ClearRenderTargetView(rtv, (FLOAT*)&clearColor);
 }
 
 void cGraphicsApiD3D11::Present() {
-	ASSERT_MSG(d3dsc != NULL, L"Need to set window for rendering");
+	ASSERT_MSG(d3dsc, L"Need to set window for rendering");
 	d3dsc->Present(0, 0);
 }
 
