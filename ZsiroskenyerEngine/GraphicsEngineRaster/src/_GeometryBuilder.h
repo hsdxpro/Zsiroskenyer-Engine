d9cfea2f0f95bf4b472c19c7f244691ab18267e6
petiaccja <kardospeter1994@hotmail.com>
2014-01-28 16:32:08 +0100

    asd

--- ZsiroskenyerEngine/GraphicsEngineRaster/src/_GeometryBuilder.h
+++ ZsiroskenyerEngine/GraphicsEngineRaster/src/_GeometryBuilder.h
@@ -10,56 +10,45 @@ Not very necessary at the moment, but implement it, because it'll be useful in t
 
 
 #include "../../Core/src/zsString.h"
-#include "../../Core/src/IGraphicsEngine.h"
 #include "Geometry.h"
-#include "../../Core/src/math/math_all.h"
-#include "ResourceReference.h"
 
 class cResourceManager;
 
 class _cGeometryBuilder {
 public:
-	struct tFace {
-		unsigned v1, v2, v3;
-		int matGroup;
-	};
 	typedef cGeometry::tMatGroup tMatGroup;
 
 	// constructors
-	_cGeometryBuilder(cResourceManager& rm);
-	_cGeometryBuilder(const _cGeometryBuilder&) = delete;
+	_cGeometryBuilder();
 	~_cGeometryBuilder();
-
-	// assignement
+	// DELETED
+	_cGeometryBuilder(const _cGeometryBuilder&) = delete;
 	_cGeometryBuilder& operator=(const _cGeometryBuilder&) = delete;
 
 	// vertex data
-	void InsertVertices(const Vec3* verts, size_t position, size_t count);
+	void SetVertexStride(size_t vSize);
+	void InsertVertices(const float* verts, size_t position, size_t count);
 	void EraseVertices(size_t position, size_t count);
 	// index data
-	void InsertFaces(tFace* faces, size_t position, size_t count);
 	void InsertIndices(unsigned* indices, size_t position, size_t count);
 	void EraseIndices(size_t position, size_t count);
 	// material groups
 	void AddMatGroup(size_t id, size_t indexOffset, size_t indexCount);
-	tMatGroup& GetMatGroup(size_t idx);
 	void EraseMatGroup(size_t idx);
 
 	// file handling
-	eGraphicsResult LoadFile(const zsString& path);
+	void LoadFile(const zsString& path);
 
 	// concat two geometries
 	_cGeometryBuilder& Concat(const _cGeometryBuilder& rhs);
-	_cGeometryBuilder& operator+=(const _cGeometryBuilder& rhs);
-	_cGeometryBuilder  operator+(const _cGeometryBuilder& rhs);
 
 	// clone geometry
 	void Clone(const _cGeometryBuilder& other);
-	void Clone(cGeometryRef geom);
+	void Clone(const cGeometry* geom);
 
 	// submit or reset geometry
-	cGeometryRef Submit(const zsString& name);
+	cGeometry* Create();
 	void Reset();
 private:
-	cResourceManager& rm;
+	
 };
