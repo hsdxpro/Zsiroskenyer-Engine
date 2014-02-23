4f1c533150cecbf5f08cb6dcc9407e74c5452b3a
petiaccja <kardospeter1994@hotmail.com>
2014-02-23 14:00:33 +0100

    lófasz rizzsel

--- ZsiroskenyerEngine/GraphicsApiD3D11/src/VertexBufferD3D11.h
+++ ZsiroskenyerEngine/GraphicsApiD3D11/src/VertexBufferD3D11.h
@@ -8,22 +8,26 @@
 
 #include "../../Core/src/IVertexBuffer.h"
 #include "../../Core/src/ResourceProperty.h"
+#include "../../Core/src/VertexFormat.h"
 
 struct ID3D11Buffer;
 
 class cVertexBufferD3D11 : public IVertexBuffer {
 public:
-	size_t GetSize() const override;
-	eUsage GetUsage() const override;
-	ID3D11Buffer* GetBufferPointer() const;
-
 	cVertexBufferD3D11(ID3D11Buffer* buffer, size_t size, eUsage usage);
 	~cVertexBufferD3D11();
 	void Release() override;
 
+	size_t GetSize() const override;
+	eUsage GetUsage() const override;
+	ID3D11Buffer* GetBufferPointer() const;
+
+	void SetFormat(cVertexFormat format) override;
+	cVertexFormat GetFormat() const override;
 public:
 	ID3D11Buffer* const buffer;
 private:
+	cVertexFormat vertexFormat;
 	size_t size;
 	eUsage usage;
 };
